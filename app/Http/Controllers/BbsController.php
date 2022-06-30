0c5e983d5cefae0f9036e1684898407ef3da6728
if164ever1 <59137538+if164ever1@users.noreply.github.com>
2022-06-30 11:00:04 +0200

    init db table

--- app/Http/Controllers/BbsController.php
+++ app/Http/Controllers/BbsController.php
@@ -6,5 +6,8 @@ use Illuminate\Http\Request;
 
 class BbsController extends Controller
 {
-    //
+    public function index(){
+        return response ('There are list of things')
+        ->header('Content0type', 'text/plain');
+    }
 }
